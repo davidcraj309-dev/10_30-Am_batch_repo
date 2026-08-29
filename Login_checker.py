4af148dc429c2b88b328f5e8f662d4c499d26f0a
davidcraj309-dev <davidcraj309@gmail.com>
2026-08-30 02:04:00 +0530

    my login commit

---
+++ Login_checker.py
@@ -0,0 +1,9 @@
+# Login Access Checker 
+
+user_name = input("Enter name: ")
+user_password = input("Enter password: ")
+
+if user_name == "david" and user_password == "1234":
+    print("Login Successful")
+else:
+    print("Login failed, Please Try again!")
